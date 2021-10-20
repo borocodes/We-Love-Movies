d2e91b356274c65667f32311465b105ba0bcacda
Jonathan Borofsky <jonborofsky@gmail.com>
2021-10-20 13:30:29 -0500

    5 tests passing

--- src/app.js
+++ src/app.js
@@ -10,11 +10,13 @@ const notFound = require("./errors/notFound");
 
 //routers
 const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
 
 app.use(errorHandler);
 app.use(notFound);
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,39 +1,41 @@
 const knex = require("../db/connection");
-const mapProperties = require("../utils/map-properties.js")
+const mapProperties = require("../utils/map-properties.js");
 
 const criticDetails = mapProperties({
-    critic_id: "critic.critic_id",
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name"
-})
+  critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+});
 
 function list() {
   return knex("movies").select("*");
 }
 
 function listTheaters(movieId) {
-    return knex("movies_theaters as mt")
-    .join("theaters as t", "mt.theater_id", "m.theater_id")
-    .select("*")
-    .where({ movie_id: movieId, is_showing: true})
+  return knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .join("theaters as t", "t.theater_id", "mt.theater_id")
+    .select("t.*", "mt.is_showing", "m.movie_id")
+    .where({ "m.movie_id": movieId })
+    .andWhere({ "mt.is_showing": true });
 }
 
 function listReviews(movieId) {
-    return knex("movies as m")
-      .join("reviews as r", "r.movie_id", "m.movie_id")
-      .join("critics as c", "c.critic_id", "r.critic_id")
-      .select("*")
-      .where({ "r.movie_id": movieId })
-      .then((reviews) => {
-        const reviewsWithCriticDetails = [];
-        reviews.forEach((review) => {
-          const addedCritic = addCriticDetails(review);
-          reviewsWithCriticDetails.push(addedCritic);
-        });
-        return reviewsWithCriticDetails;
+  return knex("movies as m")
+    .join("reviews as r", "r.movie_id", "m.movie_id")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select("*")
+    .where({ "r.movie_id": movieId })
+    .then((reviews) => {
+      const reviewsWithCriticDetails = [];
+      reviews.forEach((review) => {
+        const addedCritic = addCriticDetails(review);
+        reviewsWithCriticDetails.push(addedCritic);
       });
-  }
+      return reviewsWithCriticDetails;
+    });
+}
 
 function isShowingInTheaters() {
   return knex("movies as m")
